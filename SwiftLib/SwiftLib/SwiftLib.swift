ded4248c422de83a47dc140faa520f0c1b6f965f
Bharath <bharath2r@gmail.com>
2020-06-25 15:20:33 +0530

    Added swiftLib file.

---
+++ SwiftLib/SwiftLib/SwiftLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftLib.swift
+//  SwiftLib
+//
+//  Created by Bharath on 25/06/20.
+//  Copyright © 2020 Bharath. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
