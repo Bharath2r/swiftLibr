5983ba2e119435b36feb59af4c479cc3b1390e81
Bharath <bharath2r@gmail.com>
2020-06-25 19:04:48 +0530

    second commit.

---
+++ SwiftLib/SwiftLib/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  SwiftLib
+//
+//  Created by Bharath on 25/06/20.
+//  Copyright © 2020 Bharath. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    
+    public static func dosomething() -> String {
+        return "hi bharath, how are you!"
+    }
+}
