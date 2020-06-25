5a607607c81d27d61675c28e15c3169e0fa3f8c8
Bharath <bharath2r@gmail.com>
2020-06-25 15:12:04 +0530

    first commit

---
+++ SwiftLib/SwiftLib/SwiftLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLib.h
+//  SwiftLib
+//
+//  Created by Bharath on 25/06/20.
+//  Copyright © 2020 Bharath. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftLib.
+FOUNDATION_EXPORT double SwiftLibVersionNumber;
+
+//! Project version string for SwiftLib.
+FOUNDATION_EXPORT const unsigned char SwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLib/PublicHeader.h>
+
+
